6899f2123ee86c78c784028571fa06510dcc007f
Tairone <2006878@aluno.univesp.br>
2020-12-28 06:39:26 -0300

    First Commit

---
+++ DigitalInovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 10;
+            for (int i=0; i < numeroDeVezes; i++) {
+                Console.WriteLine($"Bem vindo ao curso de .Net {i}");
+            }
+        }
+    }
+}
